944ee5e326e1eb789661811c385f8c2b1afd121d
eliaquinmarin <126673417+eliaquinmarin@users.noreply.github.com>
2026-06-26 20:13:51 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 self.addEventListener('fetch', (event) => {
-  // Este controlador vacío es suficiente para cumplir el requisito técnico
+  // Este evento es necesario para que el navegador reconozca la app como instalable
 });
