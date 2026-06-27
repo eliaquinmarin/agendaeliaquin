acd14d7356d2dc2fa8c9d17a5672986b0cb23810
eliaquinmarin <126673417+eliaquinmarin@users.noreply.github.com>
2026-06-26 20:21:34 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,24 @@
 // sw.js
+const CACHE_NAME = 'agenda-fe-v1';
+
+// Al instalar, el SW puede guardar en caché los archivos principales
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html'
+                // Puedes agregar aquí tus otros archivos css o js
+            ]);
+        })
+    );
+});
+
+// El evento fetch es OBLIGATORIO para la instalación tipo App
 self.addEventListener('fetch', (event) => {
-  // Este evento vacío permite que el navegador reconozca la PWA como instalable
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
