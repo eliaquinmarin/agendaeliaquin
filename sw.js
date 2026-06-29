ce983e8646c28f4f8b3de17a2e9dea6a85e51e77
eliaquinmarin <126673417+eliaquinmarin@users.noreply.github.com>
2026-06-29 18:57:41 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,24 @@
 // sw.js
-const CACHE_NAME = 'agenda-fe-v1';
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
-// 1. Instalación: Fuerza la activación y guarda archivos en caché
-self.addEventListener('install', (event) => {
-    self.skipWaiting(); 
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html'
-            ]);
-        })
-    );
+firebase.initializeApp({
+  apiKey: "AIzaSyDdF_65mATjkH71yLFW97fvwQX5Sexf9Tw",
+  projectId: "notificacionesagenda",
+  messagingSenderId: "168849532431",
+  appId: "1:168849532431:web:f9552e888ece647672e57a"
 });
 
-// 2. Activación: Toma el control y LIMPIA cachés antiguas
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        console.log('SW: Eliminando caché antigua', cache);
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        }).then(() => clients.claim())
-    );
-});
+const messaging = firebase.messaging();
 
-// 3. Estrategia de Red: Prioriza internet, luego caché
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request)
-            .catch(() => {
-                return caches.match(event.request);
-            })
-    );
-});
-self.addEventListener('push', (event) => {
-    const data = event.data ? event.data.json() : { title: 'Agenda', body: 'Nuevo programa' };
-    event.waitUntil(
-        self.registration.showNotification(data.title, {
-            body: data.body,
-            icon: '/favicon.ico'
-        })
-    );
+// Manejador en segundo plano
+messaging.onBackgroundMessage((payload) => {
+  console.log('[sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/favicon.ico',
+    badge: '/favicon.ico'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
